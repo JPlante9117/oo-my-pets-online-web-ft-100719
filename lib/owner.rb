a08f329fb19103fe6e6eed7b2d007f530ccf06fb
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-10-15 18:37:54 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,5 +1,14 @@
 class Owner
-  attr_accessor
-  attr_writer 
+
   attr_reader :name, :species
+
+  def initialize(name, species)
+    @name = name
+    @species = species
+  end
+  
+  def say_species
+    
+  end
+
 end
