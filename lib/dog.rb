f3c039bec84ea86c290515846ef577f976fef1a4
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-10-15 18:55:43 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,15 @@
 class Dog
-  # code goes here
+  
+  attr_accessor :owner
+  attr_reader :name
+  
+  @@all = [] 
+  
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    
+    @@all << self
+  end
+  
 end
